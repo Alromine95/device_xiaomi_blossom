1ebd0454aefaf8adcc9a7eb85798c9b5a81327a1
Alromine95 <abhinavsingh.me5@gmail.com>
2026-09-08 16:20:36 +0530

    Update and rename lineage_blossom.mk to yaap_blossom.mk

---
+++ yaap_blossom.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/blossom/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+
+# Malloc
+PRODUCT_DISABLE_SCUDO := true
+
+# Maintainer Name
+ORION_MAINTAINER := Qbhi
+
+
+# Enable/Disable Blur
+TARGET_ENABLE_BLUR := true
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := yaap_blossom
+PRODUCT_DEVICE := blossom
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#Quicktap
+TARGET_SUPPORTS_QUICK_TAP := true
+
+#Nuke Aperture
+PRODUCT_NO_CAMERA := true
+
+#Nerver complie this
+TARGET_EXCLUDES_AUDIOFX := true
+TARGET_INCLUDE_VIA := false
+TARGET_INCLUDE_REVAMPED := false
