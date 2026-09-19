e739048b34f9010e949586e8903c5601f6a7c089
The Honored One <abhinavsingh.me5@gmail.com>
2026-09-19 17:13:18 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,10 +5,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/yaap_blossom.mk
+    $(LOCAL_DIR)/lineage_blossom.mk
 
 COMMON_LUNCH_CHOICES := \
-    yaap_blossom-user \
-    yaap_blossom-userdebug \
-    yaap_blossom-eng
+    lineage_blossom-user \
+    lineage_blossom-userdebug \
+    lineage_blossom-eng
 
